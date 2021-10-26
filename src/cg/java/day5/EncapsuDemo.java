5c17f8a24708d80be95c8adb0996d4da2f149604
Swathi435 <93043376+Swathi435@users.noreply.github.com>
2021-10-26 15:05:40 +0530

    Create EncapsuDemo.java

---
+++ src/cg/java/day5/EncapsuDemo.java
@@ -0,0 +1,40 @@
+package cg.java.day5;
+
+public class EncapsuDemo {
+	private String name;
+	private int phone;
+	private int adhar;
+	private int id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public int getAdhar() {
+		return adhar;
+	}
+	public void setAdhar(int adhar) {
+		this.adhar = adhar;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	}
+
+	
+	
+	
+	
+
+
